748dcb7a92b5739cc3d9370660e532d2047cb7db
Tobias Dély <cleverhatcamouflage@gmail.com>
2024-05-07 19:00:09 +0200

    why did i put this there

--- .config/nvim/lua/tdely/packer.lua
+++ .config/nvim/lua/tdely/packer.lua
@@ -20,7 +20,6 @@ return require('packer').startup(function(use)
 
   use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
   use('nvim-treesitter/playground')
-  use('alaviss/nim.nvim')
   use('mbbill/undotree')
   use('tpope/vim-fugitive')
   use {
