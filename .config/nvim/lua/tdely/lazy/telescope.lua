737ce7d77aa9e5c52cf617d464a2483e6e5f8037
Tobias Dély <cleverhatcamouflage@gmail.com>
2025-06-23 22:07:19 +0200

    nvim telescope-fzf-native

--- .config/nvim/lua/tdely/lazy/telescope.lua
+++ .config/nvim/lua/tdely/lazy/telescope.lua
@@ -2,10 +2,12 @@ return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
   dependencies = {
-    "nvim-lua/plenary.nvim"
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope-fzf-native.nvim"
   },
   config = function()
     require('telescope').setup({})
+    require('telescope').load_extension('fzf')
     local builtin = require('telescope.builtin')
     vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
     vim.keymap.set('n', '<leader>ph', function()
