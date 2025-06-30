b1e5d1897f4dbb6edcff3cd0308da3c51b39ec53
Tobias Dély <cleverhatcamouflage@gmail.com>
2025-06-30 22:33:54 +0200

    firefox stuff

---
+++ user-overrides.js
@@ -0,0 +1,4 @@
+/* 0=blank, 1=home, 2=last visited page, 3=resume previous session */
+user_pref("browser.startup.page", 3);
+user_pref("privacy.clearOnShutdown_v2.browsingHistoryAndDownloads", false);
+user_pref("privacy.clearOnShutdown.openWindows", false);
