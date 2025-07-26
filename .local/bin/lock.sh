9087f2be3863eecf189c5e90be5cf31186ab8348
Tobias Dély <cleverhatcamouflage@gmail.com>
2025-07-26 10:52:26 +0200

    update lock screen look

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 export XSECURELOCK_SAVER=saver_mpv
-export XSECURELOCK_LIST_VIDEOS_COMMAND="echo ~/Pictures/Wallpapers/wallpaper.png"
-export XSECURELOCK_FONT="Hack:size=11"
+export XSECURELOCK_LIST_VIDEOS_COMMAND="echo ~/Pictures/Wallpapers/wallpaper.mp4"
+export XSECURELOCK_FONT="Pixelon:size=12"
 export XSECURELOCK_AUTH_TIMEOUT=5
 export XSECURELOCK_AUTH_CURSOR_BLINK=0
 export XSECURELOCK_PASSWORD_PROMPT=time_hex
