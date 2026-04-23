d0187a7e5c1b4363712acf40424e8993613261b3
Tobias Dély <cleverhatcamouflage@gmail.com>
2026-04-23 14:37:12 +0200

    nvim 0.12 updates

--- .config/nvim/lua/tdely/lazy/treesitter.lua
+++ .config/nvim/lua/tdely/lazy/treesitter.lua
@@ -2,20 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    require('nvim-treesitter.configs').setup({
-      -- A list of parser names, or "all"
-      ensure_installed = { "c", "lua", "vim", "vimdoc", "query", "nim", "zig", "bash", "make" },
-
-      -- Install parsers synchronously (only applied to `ensure_installed`)
-      sync_install = false,
-
-      -- Automatically install missing parsers when entering buffer
-      -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-      auto_install = true,
-
-      -- List of parsers to ignore installing (or "all")
-      ignore_install = { "javascript" },
-
+    require('nvim-treesitter').setup({
       highlight = {
         enable = true,
 
@@ -34,6 +21,27 @@ return {
         -- Instead of true it can also be a list of languages
         additional_vim_regex_highlighting = false,
       },
+      indent = { enable = true, },
     })
-  end
+  end,
+  init = function()
+    local ensureInstalled = { "c", "lua", "vim", "vimdoc", "query", "nim", "zig", "bash", "make" }
+    local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+    local parsersToInstall = vim.iter(ensureInstalled)
+    :filter(function(parser)
+      return not vim.tbl_contains(alreadyInstalled, parser)
+    end)
+    :totable()
+    require('nvim-treesitter').install(parsersToInstall)
+
+    vim.api.nvim_create_autocmd('FileType', {
+      pattern = '*',
+      callback = function()
+        -- Enable treesitter highlighting and disable regex syntax
+        pcall(vim.treesitter.start)
+        -- Enable treesitter-based indentation
+        vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
+    })
+  end,
 }
