5c2075ee1fde5b632a8e12331a40e4e067e49b6e
Tobias Dély <cleverhatcamouflage@gmail.com>
2026-06-12 14:36:28 +0200

    add wallpaper selector

--- .local/bin/lock.sh
+++ .local/bin/lock.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 export XSECURELOCK_SAVER=saver_mpv
-export XSECURELOCK_LIST_VIDEOS_COMMAND="echo ~/Pictures/Wallpapers/wallpaper.png"
+export XSECURELOCK_LIST_VIDEOS_COMMAND="echo ~/.local/state/current-wallpaper"
 export XSECURELOCK_FONT="Hack:size=11"
 export XSECURELOCK_AUTH_TIMEOUT=5
 export XSECURELOCK_AUTH_CURSOR_BLINK=0
